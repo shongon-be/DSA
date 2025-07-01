103f4d693fa988cb829d4ed2c3dc9830df1354be
Hong Son <hongson530.dev@gmail.com>
2025-07-01 23:34:43 +0700

    BigO: covered definition, drop constants, O(N) & O(N^2)

---
+++ src/BigO_Notation/Main.java
@@ -0,0 +1,37 @@
+package BigO_Notation;
+
+public class Main {
+    // O(N)
+    public static void printItems(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+    }
+    // O(N^2)
+    public static void printItemsInNestedLoop (int n) {
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+                System.out.println(i + " " + j);
+            }
+
+        }
+    }
+
+
+    // Drop Constants
+    public static void printItemsInCaseOfDropConstants (int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+
+        for (int j = 0; j < n; j++) {
+            System.out.println(j);
+        }
+    }
+
+    public static void main(String[] args) {
+//        printItems(5);
+        printItemsInNestedLoop(5);
+    }
+}
