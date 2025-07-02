16a8b612049163c083500442ac8aef1b77028c67
Hong Son <hongson530.dev@gmail.com>
2025-07-02 23:31:00 +0700

    bigO: covered drop non-dominants, O(1), O(logN)

--- src/BigO_Notation/Main.java
+++ src/BigO_Notation/Main.java
@@ -1,6 +1,31 @@
 package BigO_Notation;
 
 public class Main {
+
+    // O(1)
+    public static int addItems(int n) {
+        return n + n;
+    }
+
+    // O(log N)
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
     // O(N)
     public static void printItems(int n) {
         for (int i = 0; i < n; i++) {
@@ -30,10 +55,31 @@ public class Main {
         }
     }
 
+    // Drop Non-Dominants
+    public static void printItemsInCaseOfDropNonDominants(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
+
+        for (int k = 0; k < n; k++) {
+            System.out.println(k);
+        }
+    }
+
     public static void main(String[] args) {
+        // O(1)
+        System.out.println(addItems(5));
+        // O(logN)
+        System.out.println(binarySearch(new int[] {1,2,3,4,5,6,7,8}, 7));
+        // O(N)
         printItems(5);
+        // O(N^2)
         printItemsInNestedLoop(5);
 
         printItemsInCaseOfDropConstants(5);
+        printItemsInCaseOfDropNonDominants(5);
+
     }
 }
