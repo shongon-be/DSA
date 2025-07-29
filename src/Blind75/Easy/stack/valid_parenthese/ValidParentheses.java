9082e508adeedf16f339219ea920fb0d6b6f0295
Hong Son <hongson530.dev@gmail.com>
2025-07-29 23:57:49 +0700

    Easy - Stack: completed ValidParentheses problem

---
+++ src/Blind75/Easy/stack/valid_parenthese/ValidParentheses.java
@@ -0,0 +1,35 @@
+package Blind75.Easy.stack.valid_parenthese;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    static boolean isValid(String s){
+        Stack<Character> st = new Stack<>();
+
+        for (char c : s.toCharArray()){
+            if (c == '(' || c == '{' || c == '['){
+                st.push(c);
+            }else {
+                if(st.isEmpty()) return false;
+
+                if(c == ')'){
+                    if(st.pop() != '(') return false;
+                } else if (c == ']') {
+                    if(st.pop() != '[') return false;
+                } else {
+                    if(st.pop() != '{') return false;
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "({{{{}}}))";
+
+        System.out.println(isValid(s));
+
+    }
+}
