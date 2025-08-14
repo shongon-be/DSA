d4c2b733b3661d8ae7842a2299b755dcbd45dae5
Hong Son <hongson530.dev@gmail.com>
2025-08-14 18:43:50 +0700

    Easy - Linked List: complete Linked List Cycle problem

---
+++ src/leetcode/easy/linkedlist/ll_cycle/LinkedListCycle.java
@@ -0,0 +1,39 @@
+package leetcode.easy.linkedlist.ll_cycle;
+
+
+
+public class LinkedListCycle {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    static boolean hasCycle(ListNode head) {
+        // list empty or only one node -> false
+        if (head == null || head.next == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) return true;
+        }
+
+        return false;
+    }
+}
